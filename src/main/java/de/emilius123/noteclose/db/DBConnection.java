907309571a4880793f6272d27d16758b53440628
Emil Linus Albrecht <emil.albrecht27@gmail.com>
2023-11-12 01:12:54 +0100

    Implement usage of file session handler when DB is unavailable

--- src/main/java/de/emilius123/noteclose/db/DBConnection.java
+++ src/main/java/de/emilius123/noteclose/db/DBConnection.java
@@ -5,6 +5,8 @@ import org.eclipse.jetty.server.session.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.File;
+import java.nio.file.Paths;
 import java.sql.*;
 import java.util.Properties;
 import java.util.function.Supplier;
@@ -16,6 +18,7 @@ import java.util.function.Supplier;
 public class DBConnection {
     private final MysqlDataSource dataSource;
     private Connection connection;
+    private static final File SESSION_DIR = Paths.get(System.getProperty("java.io.tmpdir"), "noteclose-sessions").toFile();
     private final Logger logger = LoggerFactory.getLogger(DBConnection.class);
 
     public DBConnection(Properties properties) {
@@ -44,6 +47,7 @@ public class DBConnection {
 
     /**
      * @return A SessionHandler that stores session data in the configured DB
+     * If not connected to DB, returns a FileSessionDataStore
      */
     public Supplier<SessionHandler> databaseSessionHandler() {
         SessionHandler sessionHandler = new SessionHandler();
@@ -53,13 +57,34 @@ public class DBConnection {
         DatabaseAdaptor adaptor = new DatabaseAdaptor();
         adaptor.setDatasource(dataSource);
         dataStoreFactory.setDatabaseAdaptor(adaptor);
-        sessionCache.setSessionDataStore(
-                dataStoreFactory.getSessionDataStore(sessionHandler)
-        );
+
+        if(connection != null) {
+            // If connected to DB, just use JDBCSessionDataStore
+            sessionCache.setSessionDataStore(
+                    dataStoreFactory.getSessionDataStore(sessionHandler)
+            );
+        } else {
+            return getFileSessionDataStore();
+        }
+
+        sessionHandler.setSessionCache(sessionCache);
+        return () -> sessionHandler;
+    }
+
+    private static Supplier<SessionHandler> getFileSessionDataStore() {
+        SessionHandler sessionHandler = new SessionHandler();
+        SessionCache sessionCache = new DefaultSessionCache(sessionHandler);
+
+        FileSessionDataStore fileSessionDataStore = new FileSessionDataStore();
+        SESSION_DIR.mkdir();
+        fileSessionDataStore.setStoreDir(SESSION_DIR);
+
+        sessionCache.setSessionDataStore(fileSessionDataStore);
         sessionHandler.setSessionCache(sessionCache);
 
         return () -> sessionHandler;
     }
+
 
     public Connection connect() {
         return connection;
